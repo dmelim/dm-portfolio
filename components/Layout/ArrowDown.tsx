284cbe947b9ca382e4a1cfd6527faefffbd4d512
dmelim <dmpmelim@gmail.com>
2022-09-14 01:39:53 +0100

    Started working on the about page.

---
+++ components/Layout/ArrowDown.tsx
@@ -0,0 +1,39 @@
+const ArrowDown = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 800 800"
+      height="100"
+      width="200"
+      className=" animate-bounce"
+    >
+      <path
+        fill="none"
+        stroke="hsl(0, 0%, 100%)"
+        strokeLinecap="square"
+        strokeWidth="11"
+        markerEnd="url(#SvgjsMarker1354)"
+        d="M283 283L517 517"
+        transform="rotate(45 385 363.787)"
+      ></path>
+      <defs>
+        <marker
+          id="SvgjsMarker1354"
+          markerHeight="7.5"
+          markerWidth="7.5"
+          orient="auto"
+          refX="3.75"
+          refY="3.75"
+          viewBox="0 0 7.5 7.5"
+        >
+          <path
+            fill="hsl(0, 0%, 100%)"
+            d="M0 7.5L2.5 3.75 0 0 7.5 3.75z"
+          ></path>
+        </marker>
+      </defs>
+    </svg>
+  );
+};
+
+export default ArrowDown;
--- components/Layout/PersonalInfo.tsx
+++ components/Layout/PersonalInfo.tsx
@@ -1,22 +1,53 @@
+import Image from "next/image";
+import { Fragment } from "react";
+import profilePic from "../../public/diogo-melim.jpg";
+import ArrowDown from "./ArrowDown";
 const PersonalInfo = () => {
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://placeimg.com/260/400/arch"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <Fragment>
+      <div className="hero min-h-screen">
+        <div className="hero-content grid grid-cols-1 ">
+          <Image
+            src={profilePic}
+            width="250"
+            height="400"
+            className="max-w-sm mask mask-circle col-span-1 hover:rotate-12 hover:mask-square duration-300 brightness-95"
+          />
+          <div className="grid justify-items-center col-span-2 row-span-3">
+            <h1 className="text-5xl font-bold text-center">
+              DIOGO
+              <span> </span>
+              <span className=" text-accent ">MELIM</span>
+            </h1>
+            <p className="py-6 text-center">
+              Web Developer based in Madeira Island
+            </p>
+            <p>See more about me and my career</p>
+            <ArrowDown />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="grid">
+        <h1>Education</h1>
+        <div>
+          <h1>Bachelors Degree</h1>
+          <p>Information</p>
+          <h1>Master's Degree</h1>
+          <p>Information</p>
+        </div>
+      </div>
+      <div className="grid">
+        <h1>Courses</h1>
+        <div>
+          <h1>React</h1>
+          <p>Information</p>
+          <h1>TypeScript</h1>
+          <p>Information</p>
+          <h1>NextJS</h1>
+          <p>Information</p>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
